91f488a99b8ec5685840dfa091cd512cb3909568
codingwithtashi <khakontas33@gmail.com>
2024-06-29 20:46:21 +0530

    updated cors

--- src/constant.ts
+++ src/constant.ts
@@ -3,4 +3,4 @@ require("dotenv").config();
 
 export const genAI = new GoogleGenerativeAI(process.env.API_KEY || "");
 export const defaultPromtMessage =
-  "Hello, I am a Tibetan user, I might ask you question related to Tibetan subject matter, I might use Tibetan phrase as well. Please keep in mind, If I ask anything please make sure to respond in English no other language including Tibetan as well.";
+  "Hello, I am a Tibetan user, I might ask you question related to Tibetan subject matter, I might use Tibetan phrase as well. Please keep in mind, If I ask anything please make sure to respond in simple plain English no other language including Tibetan as well.";
